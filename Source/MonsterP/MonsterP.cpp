cce05e4986c3bea1a169784493f0de8540765322
Tarfax <lostmike@gmail.com>
2022-03-01 18:05:54 +0100

    Project Launch!
    Yes

---
+++ Source/MonsterP/MonsterP.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MonsterP.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MonsterP, "MonsterP" );
